bfe4419ee9b57459b941e7d55398aeadb025e19e
Mike Farrelly <simplicitydevs@gmail.com>
2021-02-09 04:37:57 +0900

    Added solution to Palindrome Linked List problem

---
+++ src/dev/mikefarrelly/learn/linkedlist/classicproblems/PalindromeLinkedList.java
@@ -0,0 +1,76 @@
+package dev.mikefarrelly.learn.linkedlist.classicproblems;
+
+/**
+ * Given a singly linked list, determine if it is a palindrome.
+ *
+ * Example 1:
+ * Input: 1->2
+ * Output: false
+ *
+ * Example 2:
+ * Input: 1->2->2->1
+ * Output: true
+ *
+ * Follow up:
+ * Could you do it in O(n) time and O(1) space?
+ *
+ * https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1209/
+ */
+public class PalindromeLinkedList {
+    // Most answers are like this
+    private static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow = swap(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return true;
+    }
+
+    private static ListNode swap(ListNode head) {
+        ListNode before = null;
+        while (head != null) {
+            ListNode after = head.next;
+            head.next = before;
+            before = head;
+            head = after;
+        }
+
+        return before;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
