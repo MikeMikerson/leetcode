b2e331f6835d9377ec26966ea6a6d652d9f9f778
Mike Farrelly <simplicitydevs@gmail.com>
2021-02-16 05:58:31 +0900

    Added other solutions for Moving Average from Data Stream, #346

--- src/dev/mikefarrelly/learn/queue/MovingAverageFromDataStream.java
+++ src/dev/mikefarrelly/learn/queue/MovingAverageFromDataStream.java
@@ -37,7 +37,7 @@ public class MovingAverageFromDataStream {
         System.out.println(movingAverage.next(3)); // return 4.66667 = (1 + 10 + 3) / 3
         System.out.println(movingAverage.next(5)); // return 6.0 = (10 + 3 + 5) / 3
     }
-    
+
     private int maxSize;
     private Queue<Integer> queue = new LinkedList<>();
 
@@ -60,4 +60,102 @@ public class MovingAverageFromDataStream {
 
         return sum / queue.size();
     }
+
+//    class BestRuntime {
+//
+//        /** Initialize your data structure here. */
+//        Queue<Integer> window;
+//        double sum = 0;
+//        int size = 0;
+//
+//        public MovingAverage(int size) {
+//            window = new LinkedList<>();
+//            this.size = size;
+//        }
+//
+//        public double next(int val) { // add val to the data stream, then calculate avg of last "size" elements
+//            if(window.size() >= this.size) {
+//                int front = window.poll();
+//                sum -= front;
+//            }
+//            window.add(val);
+//            sum += val;
+//            return sum / window.size();
+//        }
+//    }
+
+//    ======================================================================
+
+//    class BestMemory {
+//        int size, head = 0, windowSum = 0, count = 0;
+//        int[] queue;
+//        public MovingAverage(int size) {
+//            this.size = size;
+//            queue = new int[size];
+//        }
+//
+//        public double next(int val) {
+//            ++count;
+//            // calculate the new sum by shifting the window
+//            int tail = (head + 1) % size;
+//            windowSum = windowSum - queue[tail] + val;
+//            // move on to the next head
+//            head = (head + 1) % size;
+//            queue[head] = val;
+//            return windowSum * 1.0 / Math.min(size, count);
+//        }
+//    }
+
+//    ======================================================================
+
+//    class AverageRuntime {
+//        int sum;
+//        int size;
+//        Deque<Integer> queue;
+//
+//        /** Initialize your data structure here. */
+//        public MovingAverage(int size) {
+//            this.size = size;
+//            this.sum = 0;
+//            this.queue = new ArrayDeque<>();
+//        }
+//
+//        public double next(int val) {
+//            queue.add(val);
+//            sum += val;
+//            if (queue.size() > size) {
+//                sum -= queue.poll();
+//            }
+//            return (double)sum/queue.size();
+//        }
+//    }
+
+//    ======================================================================
+
+//    class AverageMemory {
+//
+//        Queue<Integer> queue;
+//        int runningTotal;
+//        int size;
+//
+//        /** Initialize your data structure here. */
+//        public MovingAverage(int size) {
+//            runningTotal = 0;
+//            queue = new LinkedList<>();
+//            this.size = size;
+//        }
+//
+//        public double next(int val) {
+//            if(queue.size() == size) {
+//                int removed = queue.poll();
+//                this.runningTotal -= removed;
+//            }
+//
+//            queue.add(val);
+//            this.runningTotal += val;
+//
+//            return this.runningTotal * (1.0/Math.min(this.size, queue.size()));
+//
+//        }
+//    }
 }
