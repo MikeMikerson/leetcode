44dc9c8f47b0c7f1cc82b97103389e9a9b2e5625
Mike Farrelly <simplicitydevs@gmail.com>
2021-02-19 05:15:35 +0900

    Added BFS solution (another person's solution) do Number of Islands problem

--- src/dev/mikefarrelly/learn/queue/NumberOfIslands.java
+++ src/dev/mikefarrelly/learn/queue/NumberOfIslands.java
@@ -1,29 +1,31 @@
 package dev.mikefarrelly.learn.queue;
 
+import java.util.LinkedList;
+
 /**
  * Given an m x n 2d grid map of '1's (land) and '0's (water), return the number of islands.
- *
+ * <p>
  * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
  * You may assume all four edges of the grid are all surrounded by water.
- *
+ * <p>
  * Example 1:
  * Input: grid = [
- *   ["1","1","1","1","0"],
- *   ["1","1","0","1","0"],
- *   ["1","1","0","0","0"],
- *   ["0","0","0","0","0"]
+ * ["1","1","1","1","0"],
+ * ["1","1","0","1","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","0","0","0"]
  * ]
  * Output: 1
- *
+ * <p>
  * Example 2:
  * Input: grid = [
- *   ["1","1","0","0","0"],
- *   ["1","1","0","0","0"],
- *   ["0","0","1","0","0"],
- *   ["0","0","0","1","1"]
+ * ["1","1","0","0","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","1","0","0"],
+ * ["0","0","0","1","1"]
  * ]
  * Output: 3
- *
+ * <p>
  * Constraints:
  * - m == grid.length
  * - n == grid[i].length
@@ -78,9 +80,55 @@ public class NumberOfIslands {
         }
 
         grid[i][j] = 2;
-        dfs(grid, i-1, j);
-        dfs(grid, i+1, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i, j+1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+
+    public int bfsNumIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    bfsFill(grid, i, j);
+                    count++;
+                }
+            }
+        return count;
+    }
+
+    private void bfsFill(char[][] grid, int x, int y) {
+        grid[x][y] = '0';
+        int n = grid.length;
+        int m = grid[0].length;
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        int code = x * m + y;
+        queue.offer(code);
+        while (!queue.isEmpty()) {
+            code = queue.poll();
+            int i = code / m;
+            int j = code % m;
+            //search upward and mark adjacent '1's as '0'.
+            if (i > 0 && grid[i - 1][j] == '1') {
+                queue.offer((i - 1) * m + j);
+                grid[i - 1][j] = '0';
+            }
+            //down
+            if (i < n - 1 && grid[i + 1][j] == '1') {
+                queue.offer((i + 1) * m + j);
+                grid[i + 1][j] = '0';
+            }
+            //left
+            if (j > 0 && grid[i][j - 1] == '1') {
+                queue.offer(i * m + j - 1);
+                grid[i][j - 1] = '0';
+            }
+            //right
+            if (j < m - 1 && grid[i][j + 1] == '1') {
+                queue.offer(i * m + j + 1);
+                grid[i][j + 1] = '0';
+            }
+        }
     }
 }
