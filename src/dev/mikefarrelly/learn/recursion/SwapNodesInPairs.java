48989c4191e87526201ae99d181d1f4cc3b84d12
Mike Farrelly <simplicitydevs@gmail.com>
2021-03-10 06:20:45 +0900

    Add other solution for Swap Nodes in Pairs, #24, iterative-node swap solution

--- src/dev/mikefarrelly/learn/recursion/SwapNodesInPairs.java
+++ src/dev/mikefarrelly/learn/recursion/SwapNodesInPairs.java
@@ -1,42 +1,47 @@
 package dev.mikefarrelly.learn.recursion;
 
+import dev.mikefarrelly.learn.linkedlist.classicproblems.ReverseLinkedList;
 import dev.mikefarrelly.learn.linkedlist.singly.SinglyLinkedList;
 
 /**
  * Given a linked list, swap every two adjacent nodes and return its head.
- *
+ * <p>
  * Example 1:
  * Input: head = [1,2,3,4]
  * Output: [2,1,4,3]
- *
+ * <p>
  * Example 2:
  * Input: head = []
  * Output: []
- *
+ * <p>
  * Example 3:
  * Input: head = [1]
  * Output: [1]
- *
+ * <p>
  * Constraints:
  * - The number of nodes in the list is in the range [0, 100].
  * - 0 <= Node.val <= 100
- *
+ * <p>
  * Follow up: Can you solve the problem without modifying the values
  * in the list's nodes? (i.e., Only nodes themselves may be changed.)
- *
+ * <p>
  * https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1681/
  */
 public class SwapNodesInPairs {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         ListNode node = new ListNode(1);
         node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
         printNodes(node);
-        ListNode swappedNodes = swapPairsWithVal(node);
+        ListNode swappedNodes = swapPairs(node);
         printNodes(swappedNodes);
     }
 
-    private static void printNodes(ListNode head) {
-        ListNode cur = head;
+
+    private static void printNodes(ListNode root) {
+        ListNode cur = root;
         StringBuilder stringBuilder = new StringBuilder();
         while (cur != null) {
             stringBuilder.append(cur.val).append(" -> ");
@@ -44,9 +49,10 @@ public class SwapNodesInPairs {
         }
         stringBuilder.append("null");
         System.out.println(stringBuilder.toString());
-        System.out.println("============");    }
+        System.out.println("============");
+    }
 
-    private static ListNode swapPairsWithVal(ListNode head) {
+    private static ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
@@ -63,27 +69,32 @@ public class SwapNodesInPairs {
         return head;
     }
 
-    private static ListNode swapPairsWithNodes(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+    public static ListNode otherSolution(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode point = dummy;
+        while (point.next != null && point.next.next != null) {
+            ListNode swap1 = point.next;
+            ListNode swap2 = point.next.next;
+            point.next = swap2;
+            swap1.next = swap2.next;
+            swap2.next = swap1;
+            point = swap1;
         }
-
-        ListNode cur = head;
-
-        while (cur != null && cur.next != null) {
-            
-        }
-
-        return head;
+        return dummy.next;
     }
 
     private static class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
+
+        ListNode() {
+        }
+
         ListNode(int val) {
             this.val = val;
         }
+
         ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
