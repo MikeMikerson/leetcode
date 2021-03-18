ecc9001f1fc838a4f98e0402971131ca765f1875
Mike Farrelly <simplicitydevs@gmail.com>
2021-03-18 11:59:53 +0900

    Added other solution to Merge Two Sorted Lists, #21

--- src/dev/mikefarrelly/problems/MergeTwoSortedLists.java
+++ src/dev/mikefarrelly/problems/MergeTwoSortedLists.java
@@ -64,4 +64,37 @@ public class MergeTwoSortedLists {
 
         return dummyNode.next;
     }
+
+    public ListNode otherSolution(ListNode l1, ListNode l2)
+    {
+        ListNode sl1=l1;
+        ListNode sl2=l2;
+        ListNode head=new ListNode(0);
+        ListNode newnode=head,t;
+        while(sl1!=null && sl2!=null)
+        {
+
+            if(sl1.val<sl2.val)
+            {
+                newnode.next=sl1;
+                sl1=sl1.next;
+            }
+            else
+            {
+                newnode.next=sl2;
+                sl2=sl2.next;
+            }
+            newnode=newnode.next;
+
+        }
+        if(sl1!=null)
+        {
+            newnode.next=sl1;
+        }
+        if(sl2!=null)
+        {
+            newnode.next=sl2;
+        }
+        return head.next;
+    }
 }
