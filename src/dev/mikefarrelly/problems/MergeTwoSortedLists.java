a4d0b1f5b1ef42f28ca84d795f3fdfd8455ced2a
Mike Farrelly <simplicitydevs@gmail.com>
2021-03-18 11:57:12 +0900

    Added solution to Merge Two Sorted Lists, #21

---
+++ src/dev/mikefarrelly/problems/MergeTwoSortedLists.java
@@ -0,0 +1,67 @@
+package dev.mikefarrelly.problems;
+
+import dev.mikefarrelly.node.ListNode;
+
+/**
+ * Merge two sorted linked lists and return it as a sorted list. The list should be made by
+ * splicing together the nodes of the first two lists.
+ *
+ * Example 1:
+ * Input: l1 = [1,2,4], l2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ *
+ * Example 2:
+ * Input: l1 = [], l2 = []
+ * Output: []
+ *
+ * Example 3:
+ * Input: l1 = [], l2 = [0]
+ * Output: [0]
+ *
+ * Constraints:
+ * - The number of nodes in both lists is in the range [0, 50].
+ * - -100 <= Node.val <= 100
+ * - Both l1 and l2 are sorted in non-decreasing order.
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ */
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode mergedNode = new ListNode();
+        ListNode dummyNode = mergedNode;
+
+        while (l1 != null && l2 !=null) {
+            if (l1.val == l2.val) {
+                mergedNode.next = new ListNode(l1.val);
+                mergedNode.next.next = new ListNode(l2.val);
+                mergedNode = mergedNode.next.next;
+                l1 = l1.next;
+                l2 = l2.next;
+            } else if (l1.val < l2.val) {
+                mergedNode.next = new ListNode(l1.val);
+                mergedNode = mergedNode.next;
+                l1 = l1.next;
+            } else {
+                mergedNode.next = new ListNode(l2.val);
+                mergedNode = mergedNode.next;
+                l2 = l2.next;
+            }
+        }
+
+        if (l1 == null && l2 != null) {
+            mergedNode.next = l2;
+        } else if (l1 != null) {
+            mergedNode.next = l1;
+        }
+
+        return dummyNode.next;
+    }
+}
