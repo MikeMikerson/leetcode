5f39341eae20e903c51c773ed828e62283e4cf8d
Mike Farrelly <simplicitydevs@gmail.com>
2021-03-19 07:27:19 +0900

    Add solution to Find All Numbers Disappeared in an Array, #448

---
+++ src/dev/mikefarrelly/problems/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,31 @@
+package dev.mikefarrelly.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllNumbersDisappearedInAnArray {
+    public static void main(String[] args) {
+        int[] arr = new int[]{4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers(arr));
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> disappearedNumbers = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]);
+            val--;
+            if (nums[val] > 0) {
+                nums[val] = -nums[val];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                disappearedNumbers.add(i+1);
+            }
+        }
+
+        return disappearedNumbers;
+    }
+}
