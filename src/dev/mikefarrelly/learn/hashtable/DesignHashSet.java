2f78e14b1e47a121a91eaa443a3d0683378a75bb
Mike Farrelly <simplicitydevs@gmail.com>
2021-03-21 15:57:06 +0900

    Added other solutions for Design HashSet, #705

--- src/dev/mikefarrelly/learn/hashtable/DesignHashSet.java
+++ src/dev/mikefarrelly/learn/hashtable/DesignHashSet.java
@@ -10,14 +10,14 @@ import java.util.Queue;
  * - void add(key) Inserts the value key into the HashSet.
  * - bool contains(key) Returns whether the value key exists in the HashSet or not.
  * - void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
- *
+ * <p>
  * Example 1:
  * Input
  * ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
  * [[], [1], [2], [1], [3], [2], [2], [2], [2]]
  * Output
  * [null, null, null, true, false, null, true, null, false]
- *
+ * <p>
  * Explanation
  * MyHashSet myHashSet = new MyHashSet();
  * myHashSet.add(1);      // set = [1]
@@ -28,13 +28,13 @@ import java.util.Queue;
  * myHashSet.contains(2); // return True
  * myHashSet.remove(2);   // set = [1]
  * myHashSet.contains(2); // return False, (already removed)
- *
+ * <p>
  * Constraints:
  * - 0 <= key <= 106
  * - At most 104 calls will be made to add, remove, and contains.
- *
+ * <p>
  * Follow up: Could you solve the problem without using the built-in HashSet library?
- *
+ * <p>
  * https://leetcode.com/explore/learn/card/hash-table/182/practical-applications/1139/
  */
 public class DesignHashSet {
@@ -118,7 +118,9 @@ public class DesignHashSet {
         }
     }
 
-    /** Returns true if this set contains the specified element */
+    /**
+     * Returns true if this set contains the specified element
+     */
     public boolean contains(int key) {
         int hashedKey = hash(key);
         LinkedList<Integer> entries = set[hashedKey];
@@ -142,3 +144,101 @@ public class DesignHashSet {
         return prime;
     }
 }
+
+/**
+ * Fastest runtime
+ * <p>
+ * Best memory
+ */
+//class MyHashSet {
+//
+//    private int size = 1000;
+//    private List[] data = new List[size];
+//    /** Initialize your data structure here. */
+//    public MyHashSet() {
+//
+//    }
+//
+//    public void add(int key) {
+//        int index = key%size;
+//        if(contains(key)) return;
+//
+//        if(data[index]==null) {
+//            data[index] = new ArrayList<Integer>();
+//        }
+//        data[index].add(key);
+//    }
+//
+//    public void remove(int key) {
+//        int index = key%size;
+//        if(data[index]==null) return;
+//
+//        List<Integer> list = data[index];
+//        list.remove(Integer.valueOf(key));
+//    }
+//
+//    /** Returns true if this set contains the specified element */
+//    public boolean contains(int key) {
+//        int index = key%size;
+//        if(data[index]==null) return false;
+//
+//        List<Integer> list = data[index];
+//        return list.contains(Integer.valueOf(key));
+//    }
+//}
+
+/**
+ * Best memory
+ */
+//class MyHashSet {
+//
+//    int capacity = 0;
+//    int counter = 0;
+//    int[] set = new int[10000];
+//
+//    /** Initialize your data structure here. */
+//    public MyHashSet() {
+//
+//    }
+//
+//    public void add(int key) {
+//        if(counter >= set.length){
+//            int[] biggerSet = new int[counter*2];
+//            for (int i = 0; i < counter; i++) {
+//                biggerSet[i] = set[i];
+//            }
+//            set = biggerSet;
+//        }
+//        if(!contains(key)){
+//
+//            set[counter++] = key;
+//
+//        }
+//
+//
+//    }
+//
+//    public void remove(int key) {
+//        for(int i = 0; i < counter; i++){
+//            if(set[i] == key){
+//                for(int j = i; j < counter; j++){
+//                    set[j] = set[j+1];
+//                }
+//
+//                counter--;
+//                break;
+//            }
+//        }
+//    }
+//
+//    /** Returns true if this set contains the specified element */
+//    public boolean contains(int key) {
+//        for(int i = 0; i < counter; i++){
+//            if(set[i] == key) {
+//
+//                return true;}
+//        }
+//
+//        return false;
+//    }
+//}
