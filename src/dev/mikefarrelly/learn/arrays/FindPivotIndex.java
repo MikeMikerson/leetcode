4038ffce7188b66b01fbd763f41a687a47173e06
Mike Farrelly <simplicitydevs@gmail.com>
2021-04-08 07:31:46 +0900

    Added brute force solution to Find Pivot Index

---
+++ src/dev/mikefarrelly/learn/arrays/FindPivotIndex.java
@@ -0,0 +1,43 @@
+package dev.mikefarrelly.learn.arrays;
+
+/**
+ * https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1144/
+ */
+public class FindPivotIndex {
+    public int bruteForcePivotIndex(int[] nums) {
+        if (nums == null) {
+            return -1;
+        }
+
+        if (nums.length == 1) {
+            return 0;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int leftSum = 0;
+            for (int j = i-1; j >= 0; j--) {
+                leftSum += nums[j];
+            }
+
+            int rightSum = 0;
+            for (int j = i+1; j < nums.length; j++) {
+                rightSum += nums[j];
+            }
+
+            if (rightSum == leftSum) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public int pivotIndex(int[] nums) {
+        // [1,7,3,6,5,6]
+        /*
+         * total = 28
+         *
+         */
+        return -1;
+    }
+}
